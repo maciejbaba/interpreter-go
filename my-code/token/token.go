693a1946436086861c3df5d89bf3b3b123798997
Maciej Baba <maciek2baba2@gmail.com>
2024-10-06 22:27:51 +0200

    define some of the token types, define token type and token struct

---
+++ my-code/token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+  Type TokenType
+  Literal string
+}
+
+
+const (
+  ILLEGAL = "ILLEGAL"
+  END_OF = "END_OF"
+  
+  // Identifiers + literals
+  IDENTIFIER = "IDENTIFIER" // add, foobar, x, y - for example name of the function
+  INTEGER = "INTEGER" // 12334 - just a integer number
+
+  // Operators
+  ASSIGN = "=" 
+  PLUS = "+"
+  
+  // Delimiters
+  COMMA = ","
+  SEMICOLON = ";"
+
+  LEFT_PAREN= "("
+  RIGHT_PAREN = ")"
+  LEFT_BRACE = "{"
+  RIGHT_BRACE = "}"
+
+  // Keywords
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
