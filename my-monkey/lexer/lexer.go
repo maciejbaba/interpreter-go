d51b701c20b896f9eeb547beca87258100e688f3
Maciej Baba <maciek2baba2@gmail.com>
2024-10-19 23:11:32 +0200

    add more test cases, change input to more complex one, start expanding lexer

--- my-monkey/lexer/lexer.go
+++ my-monkey/lexer/lexer.go
@@ -1,8 +1,7 @@
 package lexer
 
 import (
-  "fmt"
-  "my-monkey/token"
+	"my-monkey/token"
 )
 
 type Lexer struct {
@@ -14,7 +13,7 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
-  lexer.readCharacter() // this will initialize all Lexer variables
+	lexer.readCharacter() // this will initialize all Lexer variables
 	return lexer
 }
 
@@ -29,38 +28,56 @@ func (lexer *Lexer) readCharacter() {
 }
 
 func (lexer *Lexer) nextToken() token.Token {
-  var currentToken token.Token
+	var currentToken token.Token
 
-  switch lexer.character {
-  case '=':
-    currentToken = newToken(token.ASSIGN, lexer.character)
-  case ';':
-    currentToken = newToken(token.SEMICOLON, lexer.character)
-  case '(':
-    currentToken = newToken(token.LEFT_PAREN, lexer.character)
-  case ')':
-    currentToken = newToken(token.RIGHT_PAREN, lexer.character)
-  case ',':
-    currentToken = newToken(token.COMMA, lexer.character)
-  case '+':
-    currentToken = newToken(token.PLUS, lexer.character)
-  case '{':
-    currentToken = newToken(token.LEFT_BRACE, lexer.character)
-  case '}':
-    currentToken = newToken(token.RIGHT_BRACE, lexer.character)
-  case ':':
-    currentToken = newToken(token.COLON, lexer.character)
-  case 0:
-    currentToken.Literal = ""
-    currentToken.Type = token.END_OF_FILE
-  }
+	switch lexer.character {
+	case '=':
+		currentToken = newToken(token.ASSIGN, lexer.character)
+	case ';':
+		currentToken = newToken(token.SEMICOLON, lexer.character)
+	case '(':
+		currentToken = newToken(token.LEFT_PAREN, lexer.character)
+	case ')':
+		currentToken = newToken(token.RIGHT_PAREN, lexer.character)
+	case ',':
+		currentToken = newToken(token.COMMA, lexer.character)
+	case '+':
+		currentToken = newToken(token.PLUS, lexer.character)
+	case '{':
+		currentToken = newToken(token.LEFT_BRACE, lexer.character)
+	case '}':
+		currentToken = newToken(token.RIGHT_BRACE, lexer.character)
+	case ':':
+		currentToken = newToken(token.COLON, lexer.character)
+	case 0:
+		currentToken.Literal = ""
+		currentToken.Type = token.END_OF_FILE
+	default:
+		if isLetter(lexer.character) {
+			currentToken.Literal = lexer.readIdentifier()
+			return currentToken
+		} else {
+			currentToken = newToken(token.ILLEGAL, lexer.character)
+		}
+	}
 
-  lexer.readCharacter()
-  return currentToken
+	lexer.readCharacter()
+	return currentToken
+}
+
+func (lexer *Lexer) readIdentifier() string {
+	startIndexOfIdentifier := lexer.position
+	for isLetter(lexer.character) { // read until encounter something different than letter
+		lexer.readCharacter()
+	}
+	endIndexOfIdentifier := lexer.position // then we have the end index of indentifier
+	return lexer.input[startIndexOfIdentifier:endIndexOfIdentifier]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, character byte) token.Token {
-  return token.Token{Type: tokenType, Literal: string(character)}
+	return token.Token{Type: tokenType, Literal: string(character)}
 }
-
-
--- my-monkey/lexer/lexer_test.go
+++ my-monkey/lexer/lexer_test.go
@@ -11,18 +11,52 @@ type TestType struct {
 }
 
 func TestNextToken(testing *testing.T) {
-	input := `=+():{},;`
+	input := `let five = 5;
+  let ten = 10;
+
+  let add = fn(x,y) {
+    x + y;
+  }
+
+  let result = add(five, ten);
+  ` 
 
 	tests := []TestType{
+    {token.LET, "let"},
+    {token.IDENTIFIER, "five"},
 		{token.ASSIGN, "="},
- 		{token.PLUS, "+"},
-		{token.LEFT_PAREN, "("},
-		{token.RIGHT_PAREN, ")"},
-    {token.COLON, ":"},
- 		{token.LEFT_BRACE, "{"},
-		{token.RIGHT_BRACE, "}"},
- 		{token.COMMA, ","},
- 		{token.SEMICOLON, ";"},
+    {token.INTEGER, "5"},
+    {token.SEMICOLON, ";"},
+    {token.LET, "let"},
+    {token.IDENTIFIER, "ten"},
+    {token.ASSIGN, "="},
+    {token.INTEGER, "10"},
+    {token.SEMICOLON, ";"},
+    {token.LET, "let"},
+    {token.IDENTIFIER, "add"},
+    {token.ASSIGN, "="},
+    {token.FUNCTION, "fn"},
+    {token.LEFT_PAREN, "("},
+    {token.IDENTIFIER, "x"},
+    {token.COMMA, ","},
+    {token.IDENTIFIER, "y"},
+    {token.RIGHT_PAREN, ")"},
+    {token.LEFT_BRACE, "{"},
+    {token.IDENTIFIER, "x"},
+    {token.PLUS, "+"},
+    {token.IDENTIFIER, "y"},
+    {token.SEMICOLON, ";"},
+    {token.RIGHT_BRACE, "}"},
+    {token.LET, "let"},
+    {token.IDENTIFIER, "result"},
+    {token.ASSIGN, "="},
+    {token.IDENTIFIER, "add"},
+    {token.LEFT_PAREN, "("},
+    {token.IDENTIFIER, "five"},
+    {token.COMMA, ","},
+    {token.IDENTIFIER, "ten"},
+    {token.RIGHT_PAREN, ")"},
+    {token.SEMICOLON, ";"},
 		{token.END_OF_FILE, ""},
 	}
 
